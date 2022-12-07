2399ed2ec818be481d2911bb5698dad41f022d13
vitaliia005 <vitaliia.savchyn.muc@gmail.com>
2022-12-07 20:03:49 +0100

    added useEffect for the forecast updating

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ForecastDay from "./ForecastDay";
 import "./Forecast.css";
 import axios from "axios";
@@ -6,10 +6,27 @@ import axios from "axios";
 export default function Forecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
   function showForecast(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
+
+  //useEffect for updating the temperatures for the forecast
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function load() {
+    const apiKey = "ca62161fa9c037c12a181a9e71f2e8ab";
+    let lat = props.coordinates.lat;
+    let lon = props.coordinates.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(showForecast);
+    return null;
+  }
+
   if (loaded) {
     console.log(forecast);
     return (
@@ -22,19 +39,14 @@ export default function Forecast(props) {
                   <ForecastDay data={dailyForecast} />{" "}
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
-    const apiKey = "ca62161fa9c037c12a181a9e71f2e8ab";
-
-    let lat = props.coordinates.lat;
-    let lon = props.coordinates.lon;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(showForecast);
-    return null;
+    load();
   }
 }
